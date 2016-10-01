2d829f5fc2e6b62b337cc42f006cc37ed9723260
ljmdullaart <ljm@acm.org>
2016-10-01 16:59:38 +0200

    added possibility to create in-files with libreoffice (spellcheck!)

--- infromodt.sh
+++ infromodt.sh
@@ -2,13 +2,13 @@
 
 #INSTALL@ /usr/local/bin/infromodt
 
-      ODTS=`ls *.odt 2> /dev/null | wc -l `
-      if [ $ODTS != 0 ] ; then
-            for FILE in *.odt ; do
-                  BASE=${FILE%%.odt}
-                  odt2txt --width=-1 $FILE > temporary.txt
-                  txt2in temporary.txt > $BASE.in
-                  rm temporary.txt
-            done
-      fi
+ODTS=`ls *.odt 2> /dev/null | wc -l `
+if [ $ODTS != 0 ] ; then
+	for FILE in *.odt ; do
+		BASE=${FILE%%.odt}
+		odt2txt --width=-1 $FILE > temporary.txt
+		txt2in temporary.txt > $BASE.in
+		rm temporary.txt
+	done
+fi
 
