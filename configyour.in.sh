e8314da2f885eb95eae7f70e4df0427fa9006992
ljmdullaart <ljm@acm.org>
2017-02-12 18:23:07 +0100

    pdf and www in sub-directories

--- configyour.in.sh
+++ configyour.in.sh
@@ -3,6 +3,10 @@
 #__IN__CONFIGYOUR__
 #VERSION 3
 
+WWWDIR=www
+PDFDIR=pdf
+EPUBDIR=epub
+
 
 banner in3 >> Makefile
 PWD=`pwd`
@@ -37,17 +41,6 @@ for argument in $* ; do
 		;;
 	(a-v*)	verbose=1
 		;;
-	(a-x*)	REMOTE_DIR="WWW/$wd"
-		HOST=shell.xs4all.nl
-		DEST=xs4all
-		;;
-	(a-z*)  REMOTE_DIR="$wd"
-		HOST=homedrive.ziggo.nl
-		DEST=ziggo
-		;;
-	(*)	echo "$0 does not understand $argument; try -h."
-		;;
-	
 	esac
 done
 
@@ -62,10 +55,21 @@ cat <<EOF
 #    wd         = $WD
 EOF
   
+ODTS=`ls *.odt 2> /dev/null | wc -l `
+if [ $ODTS != 0 ] ; then
+	for FILE in *.odt ; do
+		BASE=${FILE%%.odt}
+		odt2txt --width=-1 $FILE > temporary.txt
+		txt2in temporary.txt > $BASE.in
+		rm temporary.txt 
+	done
+fi
+
 
 INFILES=`ls *.in | egrep -v 'total.in' | sort -n | paste -sd' '`
 INFILES_noindex=`ls *.in | egrep -v 'index.in|total.in' | sort -n | paste -sd' '`
 INBASE=`ls *.in | egrep -v 'configure.in' | sort -n | sed 's/.in$//' | paste -sd' '`
+
 if [ "$INFILES" = "" ] ; then
 	echo "No .in-files found"
 	echo 'tag/in: |tag' >> Makefile
@@ -75,47 +79,49 @@ if [ "$INFILES" = "" ] ; then
 	echo '	touch tag/clean.in' >> Makefile
 	echo 'tag/upload.in: |tag' >> Makefile
 	echo '	touch tag/upload.in' >> Makefile
-else
-	echo 'tag/in: tag/in3.html tag/in3.pdf tag/in3.img' >> Makefile
-	echo '	touch tag/in' >> Makefile
-	echo 'tag/in'>>$CLEANFILE
-	echo "total.in: $INFILES_noindex" >> Makefile
-	echo "	cat $INFILES_noindex > total.in" >> Makefile
-	echo 'total.in'>>$CLEANFILE
-	#
-	#	Make some in-files that are not yet there
-	#
-	mkinheader -i > index.in
-	## odt-to-in
-	ODTS=`ls *.odt 2> /dev/null | wc -l `
-	if [ $ODTS != 0 ] ; then
-		for FILE in *.odt ; do
-			BASE=${FILE%%.odt}
-			odt2txt --width=-1 $FILE > temporary.txt
-			txt2in temporary.txt > $BASE.in
-			rm temporary.txt 
-		done
-	fi
+	exit 0
+fi
 
-###  #     #  ######   #######  #     #  
- #   ##    #  #     #  #         #   #   
- #   # #   #  #     #  #          # #    
- #   #  #  #  #     #  #####       #     
- #   #   # #  #     #  #          # #    
- #   #    ##  #     #  #         #   #   
-###  #     #  ######   #######  #     #  
 
-	echo "index.in: $INFILES_noindex" >> Makefile
-	echo "	mkinheader -i > index.in" >> Makefile
-	echo "header: $INFILES" >> Makefile
-	echo "	mkinheader -h > header" >> Makefile
-	echo 'index.in'>>$CLEANFILE
-	echo "index.htm: $INFILES" >> Makefile
-	echo "	mkinheader -h > index.htm" >> Makefile
-	echo "index.html: $INFILES" >> Makefile
-	echo "	mkinheader  > index.html" >> Makefile
-	echo 'index.htm'>>$CLEANFILE
-	echo 'index.html'>>$CLEANFILE
+
+
+
+
+echo 'tag/in: tag/in3.html tag/in3.pdf tag/in3.img' >> Makefile
+echo '	touch tag/in' >> Makefile
+echo 'tag/in'>>$CLEANFILE
+#
+#	Make some in-files that are not yet there
+#
+mkinheader -i > index.in
+## odt-to-in
+
+###  #     #  ######   #######  #     #        #######  #######  #######     #     #        
+ #   ##    #  #     #  #         #   #            #     #     #     #       # #    #        
+ #   # #   #  #     #  #          # #             #     #     #     #      #   #   #        
+ #   #  #  #  #     #  #####       #              #     #     #     #     #     #  #        
+ #   #   # #  #     #  #          # #             #     #     #     #     #######  #        
+ #   #    ##  #     #  #         #   #            #     #     #     #     #     #  #        
+###  #     #  ######   #######  #     #           #     #######     #     #     #  #######  
+
+
+#     #  #######     #     ######   #######  ######   
+#     #  #          # #    #     #  #        #     #  
+#     #  #         #   #   #     #  #        #     #  
+#######  #####    #     #  #     #  #####    ######   
+#     #  #        #######  #     #  #        #   #    
+#     #  #        #     #  #     #  #        #    #   
+#     #  #######  #     #  ######   #######  #     # 
+echo "index.in: $INFILES_noindex" >> Makefile
+echo "	mkinheader -i > index.in" >> Makefile
+echo 'index.in'>>$CLEANFILE
+echo "total.in: $INFILES_noindex" >> Makefile
+echo "	cat $INFILES_noindex > total.in" >> Makefile
+echo 'total.in'>>$CLEANFILE
+echo "header: $INFILES" >> Makefile
+echo "	mkinheader -h > header" >> Makefile
+echo 'header'>>$CLEANFILE
+
 ###  #     #   ###   
  #   ##    #  #   #  
  #   # #   #      #  
@@ -123,21 +129,29 @@ else
  #   #   # #      #  
  #   #    ##  #   #  
 ###  #     #   ###   
-	DONE_TOTAL=0
-	
-	for FILE in $INBASE ; do
-		if [ $FILE = total ] ; then DONE_TOTAL=1 ; fi
-		echo "$FILE.in3: $FILE.in" >> Makefile
-		echo "	in3 $FILE.in > $FILE.in3">>Makefile	
-		echo "$FILE.in3">>$CLEANFILE
-	done
-	if [ $DONE_TOTAL = 0 ] ; then
-		FILE=total
-
-		echo "$FILE.in3: $FILE.in" >> Makefile
-		echo "	in3 $FILE.in > $FILE.in3">>Makefile	
-		echo "$FILE.in3">>$CLEANFILE
-	fi
+#There may or may not be an index.in or total.in in this dir
+#therefore use some flags to determine whether they're done or not.
+DONE_TOTAL=0
+DONE_INDEX=0
+for FILE in $INBASE ; do
+	if [ $FILE = total ] ; then DONE_TOTAL=1 ; fi
+	if [ $FILE = index ] ; then DONE_INDEX=1 ; fi
+	echo "$FILE.in3: $FILE.in" >> Makefile
+	echo "	in3 $FILE.in > $FILE.in3">>Makefile	
+	echo "$FILE.in3">>$CLEANFILE
+done
+if [ $DONE_INDEX = 0 ] ; then
+	FILE=index
+	echo "$FILE.in3: $FILE.in" >> Makefile
+	echo "	in3 $FILE.in > $FILE.in3">>Makefile	
+	echo "$FILE.in3">>$CLEANFILE
+fi
+if [ $DONE_TOTAL = 0 ] ; then
+	FILE=total
+	echo "$FILE.in3: $FILE.in" >> Makefile
+	echo "	in3 $FILE.in > $FILE.in3">>Makefile	
+	echo "$FILE.in3">>$CLEANFILE
+fi
 
 #     #  #######  #     #  #        
 #     #     #     ##   ##  #        
@@ -147,18 +161,40 @@ else
 #     #     #     #     #  #        
 #     #     #     #     #  ####### 
 
-	INHTML=`ls *.in | grep -v 'configure.in' | sort -n | sed 's/.in$/.html/' | paste -sd' '`
-	echo "tag/in3.html: $INHTML" >> Makefile
-	echo "	touch tag/in3.html" >> Makefile
-	echo "tag/in3.html">>$CLEANFILE
-	
-	for FILE in $INBASE ; do
-		echo "$FILE.html: $FILE.in3 header">>Makefile
-		echo "	in3html $FILE.in3 > $FILE.html">>Makefile
-		echo "$FILE.html">>$CLEANFILE
-		echo "$FILE.html">>$UPLOADFILE
-	done
-	
+DONE_TOTAL=0
+DONE_INDEX=0
+
+INHTML=`ls *.in | sort -n | sed 's/.in$/.html/' |sed "s/^/$WWWDIR\//"| paste -sd' '`
+if [ ! -f index.in ] ; then INHTML="INHTML $WWWDIR/index.html" ; fi
+if [ ! -f total.in ] ; then INHTML="INHTML $WWWDIR/total.html" ; fi
+echo "tag/in3.html: $INHTML" >> Makefile
+echo "	touch tag/in3.html" >> Makefile
+echo "tag/in3.html">>$CLEANFILE
+
+add_www(){
+	echo "$WWWDIR/$1.html: $1.in3 header |$WWWDIR ">>Makefile
+	echo "	in3html $1.in3 > $WWWDIR/$1.html">>Makefile
+	echo "$WWWDIR/$1.html">>$CLEANFILE
+	echo "$WWWDIR/$1.html">>$UPLOADFILE
+}
+for FILE in $INBASE ; do
+	if [ $FILE = total ] ; then DONE_TOTAL=1 ; fi
+	if [ $FILE = index ] ; then DONE_INDEX=1 ; fi
+	add_www $FILE
+done
+echo "index.htm: $INFILES" >> Makefile
+echo "	mkinheader -h > index.htm" >> Makefile
+echo 'index.htm'>>$CLEANFILE
+
+if [ $DONE_INDEX = 0 ] ; then
+	add_www index
+fi
+if [ $DONE_TOTAL = 0 ] ; then
+	add_www total
+fi
+
+echo "www:" >> Makefile
+echo "	mkdir www" >> Makefile
 ######   ######   #######  
 #     #  #     #  #        
 #     #  #     #  #        
@@ -167,45 +203,44 @@ else
 #        #     #  #        
 #        ######   #     
 
-	INPDF=`ls *.in | grep -v 'configure.in'| grep -vi 'index.in' | grep -v total.in | sort -n | sed 's/.in$/.pdf/' | paste -sd' '`
-	echo "tag/in3.pdf: total.pdf $INPDF" >> Makefile
-	echo "	touch tag/in3.pdf" >> Makefile
-	DONE_TOT=0
-	for FILE in $INBASE ; do
-		if [ $FILE = total ] ; then DONE_TOT=1 ; fi
-		echo "$FILE.pdf: $FILE.ps">>Makefile
-		echo "	cat $FILE.ps | ps2pdf - - > $FILE.pdf">>Makefile
-		echo "$FILE.ps: $FILE.min">>Makefile
-		echo "	groff -min $FILE.min > $FILE.ps">>Makefile
-		echo "$FILE.min: $FILE.tbl">>Makefile
-		echo "	tbl $FILE.tbl > $FILE.min">>Makefile
-		echo "$FILE.tbl: $FILE.in3 tag/in3.img">>Makefile
-		echo "	in3tbl $FILE.in3 > $FILE.tbl">>Makefile
-		echo "$FILE.pdf">>$CLEANFILE
-		echo "$FILE.pdf">>$UPLOADFILE
-		echo "$FILE.ps">>$CLEANFILE
-		echo "$FILE.min">>$CLEANFILE
-		echo "$FILE.tbl">>$CLEANFILE
-	done
-	if [ $DONE_TOT = 0 ] ; then
-		FILE=total
-		echo "$FILE.pdf: $FILE.ps">>Makefile
-		echo "	cat $FILE.ps | ps2pdf - - > $FILE.pdf">>Makefile
-		echo "$FILE.ps: $FILE.min">>Makefile
-		echo "	groff -min $FILE.min > $FILE.ps">>Makefile
-		echo "$FILE.min: $FILE.tbl">>Makefile
-		echo "	tbl $FILE.tbl > $FILE.min">>Makefile
-		echo "$FILE.tbl: $FILE.in3 tag/in3.img">>Makefile
-		echo "	in3tbl $FILE.in3 > $FILE.tbl">>Makefile
-		echo "$FILE.pdf">>$CLEANFILE
-		echo "$FILE.pdf">>$UPLOADFILE
-		echo "$FILE.ps">>$CLEANFILE
-		echo "$FILE.min">>$CLEANFILE
-		echo "$FILE.tbl">>$CLEANFILE
-	fi
-		
-		
+INPDF=`ls *.in | sort -n | sed "s/.in$/.pdf/;s/^/$PDFDIR\//" | paste -sd' '`
+echo "tag/in3.pdf: $PDFDIR/total.pdf $INPDF" >> Makefile
+echo "	touch tag/in3.pdf" >> Makefile
+DONE_TOTAL=0
+DONE_INDEX=0
+ 
+add_pdf(){
+	echo "$PDFDIR/$1.pdf: $PDFDIR/$1.ps">>Makefile
+	echo "	cat $PDFDIR/$1.ps | ps2pdf - - > $PDFDIR/$1.pdf">>Makefile
+	echo "$PDFDIR/$1.ps: $PDFDIR/$1.min">>Makefile
+	echo "	groff -min $PDFDIR/$1.min > $PDFDIR/$1.ps">>Makefile
+	echo "$PDFDIR/$1.min: $PDFDIR/$1.tbl">>Makefile
+	echo "	tbl $PDFDIR/$1.tbl > $PDFDIR/$1.min">>Makefile
+	echo "$PDFDIR/$1.tbl: $1.in3 tag/in3.img |$PDFDIR">>Makefile
+	echo "	in3tbl $1.in3 > $PDFDIR/$1.tbl">>Makefile
+	echo "$PDFDIR/$1.pdf">>$CLEANFILE
+	echo "$PDFDIR/$1.pdf">>$UPLOADFILE
+	echo "$PDFDIR/$1.ps">>$CLEANFILE
+	echo "$PDFDIR/$1.min">>$CLEANFILE
+	echo "$PDFDIR/$1.tbl">>$CLEANFILE
+}
+ 
+for FILE in $INBASE ; do
+	if [ $FILE = total ] ; then DONE_TOTAL=1 ; fi
+	if [ $FILE = index ] ; then DONE_INDEX=1 ; fi
+	add_pdf $FILE
+done
+if [ $DONE_TOTAL = 0 ] ; then
+	add_pdf total
+fi
+if [ $DONE_INDEX = 0 ] ; then
+	add_pdf index
+fi
 	
+echo "pdf:" >> Makefile
+echo "	mkdir pdf" >> Makefile
+	
+
 
 
 ###  #     #     #      #####   #######   #####   
@@ -215,31 +250,48 @@ else
  #   #     #  #######  #     #  #              #  
  #   #     #  #     #  #     #  #        #     #  
 ###  #     #  #     #   #####   #######   #####   
+ALLIMAGE=""
+
+mk_images(){
+	BASEIMAGE=${IMAGE%.*}
+	ALLIMAGE="$ALLIMAGE $IMAGE $BASEIMAGE.eps"
+	if [ -f $BASEIMAGE.xcf ] ; then
+		echo "$IMAGE: $BASEIMAGE.xcf" >> Makefile
+		echo "	convert $BASEIMAGE.xcf $IMAGE" >> Makefile
+		echo "$IMAGE">>$CLEANFILE
+		echo "$IMAGE">>$UPLOADFILE
+	fi
+	if [ -f $BASEIMAGE.dia ] ; then
+		echo "$IMAGE: $BASEIMAGE.dia" >> Makefile
+		echo "	dia --export=$IMAGE $BASEIMAGE.dia" >> Makefile
+		echo "$IMAGE">>$CLEANFILE
+		echo "$IMAGE">>$UPLOADFILE
+	fi
+	echo "$BASEIMAGE.eps: $IMAGE" >> Makefile
+	echo "	convert $IMAGE $BASEIMAGE.eps" >> Makefile
 	
-	cat $INFILES | sed -n 's/^\.img *//p' | sed 's/ .*//'|
-	while read IMAGE; do
-		BASEIMAGE=${IMAGE%.*}
-		ALLIMAGE="$ALLIMAGE $IMAGE"
-		if [ -f $BASEIMAGE.xcf ] ; then
-			echo "$IMAGE: $BASEIMAGE.xcf" >> Makefile
-			echo "	convert $BASEIMAGE.xcf $IMAGE" >> Makefile
-			echo "$IMAGE">>$CLEANFILE
-			echo "$IMAGE">>$UPLOADFILE
-		fi
-		if [ -f $BASEIMAGE.dia ] ; then
-			echo "$IMAGE: $BASEIMAGE.dia" >> Makefile
-			echo "	dia --export=$IMAGE $BASEIMAGE.dia" >> Makefile
-			echo "$IMAGE">>$CLEANFILE
-			echo "$IMAGE">>$UPLOADFILE
-		fi
-	done
-	sed -n  's/^.map *image *//p' *in >>$UPLOADFILE
-	sed -n  's/^\.img *//p' *in | sed 's/ .*//' >>$UPLOADFILE
-	IMGFILES=`cat $INFILES | sed -n 's/^.img //p'  | sed 's/ .*//'| paste -sd ' '`
-	echo "tag/in3.img: $IMGFILES |tag" >> Makefile
-	echo "	touch tag/in3.img" >> Makefile
-	echo "tag/in3.img">>$CLEANFILE
-	# remark: eps files are done by in3tbl
+}
+
+cat $INFILES | sed -n 's/^\.img *//p' | sed 's/ .*//'|sort -u|
+while read IMAGE; do
+	mk_images
+done
+
+cat $INFILES | sed -n 's/^\.map image *//p' | sed 's/ .*//'|sort -u|
+while read IMAGE; do
+	mk_images
+done
+
+IMGFILES=`cat $INFILES | sed -n 's/^.img //p'  | sed 's/ .*//'| paste -sd ' '`
+MAPFILES=`cat $INFILES | sed -n 's/^.map image *//p' | sed 's/ .*//'| paste -sd ' '`
+
+echo "tag/in3.img: $IMGFILES $MAPFILES |tag" >> Makefile
+echo "	cp $IMGFILES $MAPFILES $WWWDIR">> Makefile
+echo "	touch tag/in3.img" >> Makefile
+echo "tag/in3.img">>$CLEANFILE
+
+sed -n  "s/^.map *image */$WWWDIR\//p" *in >>$UPLOADFILE
+sed -n  "s/^\.img */$WWWDIR\//p" *in | sed 's/ .*//' >>$UPLOADFILE
 
 
  #####   ######   #######           #      #####          #####    #####   #     #  ######    #####   #######  
@@ -250,18 +302,18 @@ else
 #     #  #     #     #           #     #  #     #        #     #  #     #  #     #  #    #   #     #  #        
  #####   ######      #           #     #   #####          #####    #####    #####   #     #   #####   #######  
 
-	## odt-to-in
-	ODTS=`ls *.odt 2> /dev/null | wc -l `
-	if [ $ODTS != 0 ] ; then
-		for FILE in *.odt ; do
-			BASE=${FILE%%.odt}
-			echo "$BASE.in: $FILE" >> Makefile
-			echo "	odt2txt --width=-1 $FILE > temporary.txt" >> Makefile
-			echo "	txt2in temporary.txt > $BASE.in" >> Makefile
-			echo "	rm temporary.txt " >> Makefile
-			echo "$BASE.in">>$CLEANFILE
-		done
-	fi
+## odt-to-in
+ODTS=`ls *.odt 2> /dev/null | wc -l `
+if [ $ODTS != 0 ] ; then
+	for FILE in *.odt ; do
+		BASE=${FILE%%.odt}
+		echo "$BASE.in: $FILE" >> Makefile
+		echo "	odt2txt --width=-1 $FILE > temporary.txt" >> Makefile
+		echo "	txt2in temporary.txt > $BASE.in" >> Makefile
+		echo "	rm temporary.txt " >> Makefile
+		echo "$BASE.in">>$CLEANFILE
+	done
+fi
 
  #####   #        #######     #     #     #  
 #     #  #        #          # #    ##    #  
@@ -271,16 +323,16 @@ else
 #     #  #        #        #     #  #    ##  
  #####   #######  #######  #     #  #     #  
 
-	echo "tag/clean.in:" >> Makefile
-	cat $CLEANFILE | while read F ; do
-		echo "	rm -f $F" >> Makefile
-	done
-	echo "	touch tag/clean.in" >> Makefile
+echo "tag/clean.in:" >> Makefile
+cat $CLEANFILE | while read F ; do
+	echo "	rm -f $F" >> Makefile
+done
+echo "	touch tag/clean.in" >> Makefile
 
-	rm -f $CLEANFILE
-		
+rm -f $CLEANFILE
 	
-	
+
+
 
 #     #  ######   #         #####      #     ######   
 #     #  #     #  #        #     #    # #    #     #  
@@ -289,27 +341,26 @@ else
 #     #  #        #        #     #  #######  #     #  
 #     #  #        #        #     #  #     #  #     #  
  #####   #        #######   #####   #     #  ######   
-	if [ -f destination ] ; then
-		if [ -f stylesheet.css ] ; then
-			echo stylesheet.css >> $UPLOADFILE
-		fi
-		if [ -f UPLOAD ] ; then
-			UPLOADFILES=`cat $UPLOADFILE UPLOAD | paste -sd' '`
-		else
-			UPLOADFILES=`cat $UPLOADFILE | paste -sd' '`
-		fi
-		echo "tag/upload.in: $UPLOADFILES |tag">>Makefile
-		echo "	upload_all $UPLOADFILES">>Makefile
-		echo "	touch tag/upload.in">>Makefile
-	else
-		echo "tag/upload.in: |tag">>Makefile
-		echo "	echo 'The following files would qualify for upload if a destination file would exist:'">>Makefile
-		echo  -n "	ls -l ">>Makefile
-		grep -v index.pdf  $UPLOADFILE | paste -sd' '  >>Makefile
-		echo "	touch tag/upload.in">>Makefile
+if [ -f destination ] ; then
+	if [ -f stylesheet.css ] ; then
+		echo stylesheet.css >> $UPLOADFILE
 	fi
-
+	if [ -f UPLOAD ] ; then
+		UPLOADFILES=`cat $UPLOADFILE UPLOAD | paste -sd' '`
+	else
+		UPLOADFILES=`cat $UPLOADFILE | paste -sd' '`
+	fi
+	echo "tag/upload.in: $UPLOADFILES |tag">>Makefile
+	echo "	upload_all -f  $UPLOADFILES">>Makefile
+	echo "	touch tag/upload.in">>Makefile
+else
+	echo "tag/upload.in: |tag">>Makefile
+	echo "	echo 'The following files would qualify for upload if a destination file would exist:'">>Makefile
+	echo  -n "	ls -l ">>Makefile
+	grep -v index.pdf  $UPLOADFILE | paste -sd' '  >>Makefile
+	echo "	touch tag/upload.in">>Makefile
 fi
+
 rm -f $CLEANFILE
 rm -f $UPLOADFILE
 exit 0
