754fcb3c31716b0e830b8bd62f361c8c7b90beae
ljmdullaart <ljm@acm.org>
2021-02-14 12:54:35 +0100

    Don't execute if in3xml directory exists.

--- configyour.in.sh
+++ configyour.in.sh
@@ -72,10 +72,23 @@ fi
 
 
 INFILES=$(ls *.in  2> /dev/null | egrep -v 'total.in|complete.in' | sort -n | paste -sd' ')
-INFILES_noindex=$(ls *.in  2> /dev/null | egrep -v 'index.in|total.in|complete.in' | sort -n | paste -sd' ')
-INBASE=$(ls *.in  2> /dev/null | egrep -v 'configure.in|complete.in' | sort -n | sed 's/.in$//' | paste -sd' ')
+INFILES_noindex=$(ls *.in  2> /dev/null | egrep -v 'index.in|total.in|complete.in'| sort -n | paste -sd' ')
+INBASE=$(ls *.in  2> /dev/null | egrep -v 'configure.in' | sort -n | sed 's/.in$//' | paste -sd' ')
 
 if [ "$INFILES" = "" ] ; then
+	echo "No .in-files found" >>$LOG
+	echo 'tag/in: |tag' >> Makefile
+	echo '	touch tag/in' >> Makefile
+	echo 'tag/in'>>$CLEANFILE
+	echo 'tag/clean.in: |tag' >> Makefile
+	echo '	touch tag/clean.in' >> Makefile
+	echo 'tag/upload.in: |tag' >> Makefile
+	echo '	touch tag/upload.in' >> Makefile
+	echo "configour.in3 finishing" >>$LOG
+	exit 0
+fi
+
+if [ -d in3xml ] ; then
 	echo "No .in-files found" >>$LOG
 	echo 'tag/in: |tag' >> Makefile
 	echo '	touch tag/in' >> Makefile
@@ -130,9 +143,13 @@ fi
 if [ -f index.bottom ] ; then
 	index_top_bottom="$index_top_bottom index.bottom"
 fi
-echo "index.in: $INFILES_noindex $index_top_bottom" >> Makefile
-echo "	mkinheader -i > index.in" >> Makefile
-echo 'index.in'>>$CLEANFILE
+if grep "index.in:" Makefile >> $LOG ; then
+	echo "Someone already made index.in" >> $LOG
+else
+	echo "index.in: $INFILES_noindex $index_top_bottom" >> Makefile
+	echo "	mkinheader -i > index.in" >> Makefile
+	echo 'index.in'>>$CLEANFILE
+fi
 echo "total.in: $INFILES_noindex" >> Makefile
 echo "	grep -vh '^\.header'  $INFILES_noindex > total.in" >> Makefile
 echo 'total.in'>>$CLEANFILE
@@ -188,8 +205,8 @@ if [ -d $HTMLDIR ] ; then
 	if [ ! -f total.in ] ; then INHTML="$INHTML $HTMLDIR/total.html" ; fi
 	echo "tag/in3.html: $INHTML $HTMLDIR/header.htm" >> Makefile
 
-	echo "	@cp block_*.png $HTMLDIR || echo 'no blocks to copy'" >> Makefile
-	echo "	@cp block/*.png $HTMLDIR || echo 'no blocks in dir to copy'" >> Makefile
+	echo "	@cp block_*.png $HTMLDIR 2>/dev/null || echo 'no blocks to copy'" >> Makefile
+	echo "	@cp block/*.png $HTMLDIR 2>/dev/null || echo 'no blocks in dir to copy'" >> Makefile
 	echo "	touch tag/in3.html" >> Makefile
 	echo "tag/in3.html">>$CLEANFILE
 	
@@ -237,7 +254,7 @@ if [ -d $WWWDIR ] ; then
 
 	if [ -f stylesheet.css ] ; then
 		echo "$WWWDIR/stylesheet.css: stylesheet.css |$WWWDIR ">>Makefile
-		echo "	@cp stylesheet.css $WWWDIR || echo 'no stylesheet to copy'">>Makefile
+		echo "	@cp stylesheet.css $WWWDIR 2>/dev/null || echo 'no stylesheet to copy'">>Makefile
 		echo "$WWWDIR/stylesheet.css">>$CLEANFILE
 		echo "$WWWDIR/stylesheet.css">>$UPLOADFILE
 	fi
@@ -245,10 +262,10 @@ if [ -d $WWWDIR ] ; then
 	add_www(){
 		echo "$WWWDIR/$1.html: $1.in3 header |$WWWDIR ">>Makefile
 		echo "	in3html $1.in3 > $WWWDIR/$1.html">>Makefile
-		echo "	@cp block/*.png $WWWDIR || echo 'No blocks to copy'" >> Makefile
+		echo "	@cp block/*.png $WWWDIR 2>/dev/null || echo 'No blocks to copy'" >> Makefile
 	#	if grep '^\.BLOCK' $1.in*  > /dev/null ; then
-	#		echo "	@cp block_*.png $WWWDIR || echo 'No blocks to copy'" >> Makefile
-	#		echo "	@cp block/*.png $WWWDIR || echo 'No blocks in dir to copy'" >> Makefile
+	#		echo "	@cp block_*.png $WWWDIR 2>/dev/null || echo 'No blocks to copy'" >> Makefile
+	#		echo "	@cp block/*.png $WWWDIR  2>/dev/null || echo 'No blocks in dir to copy'" >> Makefile
 	#		echo "$WWWDIR/block_*png">>$CLEANFILE
 	#	fi
 		echo "$WWWDIR/$1.html">>$CLEANFILE
@@ -309,12 +326,12 @@ if [ -d $EPUBDIR ] ; then
 	echo "tag/in3.epub: $EPUBDIR/$WD.epub" >> Makefile
 	echo "	touch tag/in3.epub" >> Makefile
 	echo "$EPUBDIR/$WD.epub: $INHTML $EPUBDIR/index.html" >> Makefile
-	echo "	@cp *.png $EPUBDIR|| echo 'no png to copy'" >> Makefile
-	echo "	@cp block/*.png $EPUBDIR|| echo 'no png from blockdir to copy'" >> Makefile
+	echo "	@cp *.png $EPUBDIR 2>/dev/null || echo 'no png to copy'" >> Makefile
+	echo "	@cp block/*.png $EPUBDIR 2>/dev/null || echo 'no png from blockdir to copy'" >> Makefile
 	if [ -f epubstylesheet.css ] ; then
-		echo "	@cp epubstylesheet.css $EPUBDIR/stylesheet.css||echo 'no epub stylesheet to copy'" >> Makefile
+		echo "	@cp epubstylesheet.css $EPUBDIR/stylesheet.css 2>/dev/null ||echo 'no epub stylesheet to copy'" >> Makefile
 	elif [ -f stylesheet.css ] ; then
-		echo "	@cp stylesheet.css $EPUBDIR||echo 'no epub stylesheet to copy'" >> Makefile
+		echo "	@cp stylesheet.css $EPUBDIR 2>/dev/null ||echo 'no epub stylesheet to copy'" >> Makefile
 	fi
 	echo "	cd $EPUBDIR ; ebook-convert index.html $WD.epub $pub_opt" >> Makefile
 	echo "tag/in3.epub">>$CLEANFILE
@@ -453,11 +470,11 @@ mk_images(){
 		echo "	convert $BASEIMAGE.xcf $IMAGE" >> Makefile
 		echo "$IMAGE">>$CLEANFILE
 		echo "$IMAGE">>$UPLOADFILE
-	elif [ -f $BASEIMAGE.dia ] ; then
-		echo "$IMAGE: $BASEIMAGE.dia" >> Makefile
-		echo "	dia --export=$IMAGE $BASEIMAGE.dia" >> Makefile
-		echo "$IMAGE">>$CLEANFILE
-		echo "$IMAGE">>$UPLOADFILE
+	#elif [ -f $BASEIMAGE.dia ] ; then
+		#echo "$IMAGE: $BASEIMAGE.dia" >> Makefile
+		#echo "	dia --export=$IMAGE $BASEIMAGE.dia" >> Makefile
+		#echo "$IMAGE">>$CLEANFILE
+		#echo "$IMAGE">>$UPLOADFILE
 	elif [ -f $BASEIMAGE.gnuplot ] ; then
 		echo "$IMAGE: $BASEIMAGE.gnuplot" >> Makefile
 		echo "	gnuplot $BASEIMAGE.gnuplot" >> Makefile
@@ -477,7 +494,7 @@ mk_images(){
 	
 }
 
-CURIMG=$(cat $INFILES | sed -n 's/^\.img *//p' | sed 's/ .*//'|sort -u)
+CURIMG=$(cat $INFILES | sed -n 's/ LEFT / /; s/ RIGHT / /;s/^\.img *//p' | sed 's/ .*//'|sort -u)
 for IMAGE in $CURIMG ; do
 	mk_images
 	BASEIMAGE=${IMAGE%.*}
@@ -507,13 +524,13 @@ done
 
 echo "tag/in3.img: $ALLIMAGE|tag" >> Makefile
 if [ -d $EPUBDIR ] ; then
-	echo "	@cp $ALLIMAGE $EPUBDIR || echo 'No files to copy'">> Makefile
+	echo "	@cp $ALLIMAGE $EPUBDIR  2>/dev/null || echo 'No files to copy'">> Makefile
 	echo "	rm -f $EPUBDIR/*.eps">> Makefile
 
 fi
 if [ -d $WWWDIR ] ; then
 	if [ "$ALLIMAGE" != "" ] ; then
-		echo "	@cp $ALLIMAGE $WWWDIR || echo 'No files to copy'">> Makefile
+		echo "	@cp $ALLIMAGE $WWWDIR  2>/dev/null || echo 'No files to copy'">> Makefile
 		echo "	rm -f $WWWDIR/*.eps">> Makefile
 	fi
 fi
@@ -559,6 +576,7 @@ cat $CLEANFILE | while read F ; do
 	echo "	rm -f $F" >> Makefile
 done
 echo "	rm -f block/*"  >> Makefile
+echo "	rm -f www/*"  >> Makefile
 
 echo "	rm -f block_* */block_*" >> Makefile
 echo "	touch tag/clean.in" >> Makefile
